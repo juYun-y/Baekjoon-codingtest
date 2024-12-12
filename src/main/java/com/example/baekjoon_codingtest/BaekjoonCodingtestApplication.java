a3b66df449b09a0466ca9618c4b3351dbcde0182
juyoung <rachel7631@naver.com>
2024-12-12 16:43:30 +0900

    테스트용 커밋

---
+++ src/main/java/com/example/baekjoon_codingtest/BaekjoonCodingtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.baekjoon_codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekjoonCodingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaekjoonCodingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baekjoon_codingtest/BaekjoonCodingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baekjoon_codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekjoonCodingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
